5caa40cbb9d7cf0ea32e949d9b80b7c7f2522d26
Wojtek <wojtek@MacBook-Pro-Wojtek.local>
2023-09-05 16:40:32 +0200

    add guard for formatting

---
+++ config/nvim/lua/wk/plugins/guard.lua
@@ -0,0 +1,16 @@
+return {
+  'nvimdev/guard.nvim',
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local ft = require('guard.filetype');
+
+    ft('lua'):fmt('stylua')
+
+    require('guard').setup({
+      -- the only options for the setup function
+      fmt_on_save = true,
+      -- Use lsp if no formatter was defined for this filetype
+      lsp_as_default_formatter = true,
+    })
+  end
+}

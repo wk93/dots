88fb260c0b3297382959a56ae4b5c682f9080c37
Wojtek <wojtek@MacBook-Pro-Wojtek.local>
2023-09-05 16:18:41 +0200

    add treesitter

---
+++ config/nvim/lua/wk/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require 'nvim-treesitter.configs'.setup {
+      auto_install = true,
+      indent = {
+        enable = true
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+      highlight = {
+        enable = true,
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+        additional_vim_regex_highlighting = false,
+      }
+    }
+  end
+}

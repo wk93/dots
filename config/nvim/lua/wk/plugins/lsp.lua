99ac0d44359e1e2954b93a3ae957cf5ab11c4704
Wojtek <wojtek@MacBook-Pro-Wojtek.local>
2023-09-05 18:44:57 +0200

    add autopairs tag

--- config/nvim/lua/wk/plugins/lsp.lua
+++ config/nvim/lua/wk/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
 			"mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
 			"hrsh7th/nvim-cmp",
-			"NvChad/nvim-colorizer.lua",
 		},
 		config = function()
 			require("neodev").setup({

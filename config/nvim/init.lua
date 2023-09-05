2a3e32d82ff852c01231b141e750fbb1f78db5f3
Wojtek <wojtek@MacBook-Pro-Wojtek.local>
2023-09-05 10:19:00 +0200

    config file Explorer

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,16 @@
 vim.g.mapleader = " "
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 30
+
+local function map(mode, shortcut, command)
+	vim.keymap.set(mode, shortcut, command, {noremap=true})
+end
+
+local function nmap(shortuct, command)
+	map('n', shortuct, command)
+end
+
+nmap('<leader><leader>', ':Lexplore<CR>')
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then

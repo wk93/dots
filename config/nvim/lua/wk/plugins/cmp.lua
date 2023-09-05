e51ad33fb5fbe15572eb7468736edeeb8426bf87
Wojtek <wojtek@MacBook-Pro-Wojtek.local>
2023-09-05 16:26:58 +0200

    use luasnip in cmp

--- config/nvim/lua/wk/plugins/cmp.lua
+++ config/nvim/lua/wk/plugins/cmp.lua
@@ -13,7 +13,14 @@ local M = {
 
 M.config = function()
   local cmp = require("cmp")
+  local luasnip = require("luasnip")
   vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+  local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
 
   cmp.setup({
     snippet = {
@@ -33,8 +40,8 @@ M.config = function()
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif vim.fn["vsnip#available"](1) == 1 then
-          feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
         elseif has_words_before() then
           cmp.complete()
         else
@@ -44,8 +51,10 @@ M.config = function()
       ["<S-Tab>"] = cmp.mapping(function()
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-          feedkey("<Plug>(vsnip-jump-prev)", "")
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
         end
       end, { "i", "s" }),
     },

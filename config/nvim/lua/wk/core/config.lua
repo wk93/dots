c19a4eb6e24145d08d58c418020bda2fac56812f
Wojtek <wojtek@MacBook-Pro-Wojtek.local>
2023-09-05 11:17:18 +0200

    set numbers and tabs

--- config/nvim/lua/wk/core/config.lua
+++ config/nvim/lua/wk/core/config.lua
@@ -2,3 +2,16 @@ vim.g.mapleader = " "
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
 vim.g.netrw_keepdir = 0
+
+-- Numbers
+vim.opt.number = true
+vim.opt.numberwidth = 2
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+
+-- Indenting
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
